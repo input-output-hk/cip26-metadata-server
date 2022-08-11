b6c8dbe0346e2e0302103d19873f3a0cae7a5774
Alonso Gomez <luisalonso.gomez@globant.com>
2022-08-11 09:33:10 -0700

    feat: unit test configuration (more fixes to MR comments)

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,6 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
     verbose: true,
-    transform: {
-        '^.+\\.ts?$': 'ts-jest',
-    }
+    preset: 'ts-jest'
 };
 export default config;
